bd29d33ae47312f7f6376a0f2ddfb32ea10b29fc
unknown <zahidayub924@gmail.com>
2020-02-15 19:03:21 +0500

    Updates

---
+++ precache-manifest.e89d565963ee02ecb775ca88bc43f00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6cf30abba6c04dfbda3e4335c97928",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "90db5e6dc73a23922024",
+    "url": "/deploy-react/static/css/main.8aa270f3.chunk.css"
+  },
+  {
+    "revision": "65cc8e33be9473092176",
+    "url": "/deploy-react/static/js/2.7c062e74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-react/static/js/2.7c062e74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90db5e6dc73a23922024",
+    "url": "/deploy-react/static/js/main.a2440215.chunk.js"
+  },
+  {
+    "revision": "06287f34e9657945e24e",
+    "url": "/deploy-react/static/js/runtime-main.a8921ece.js"
+  }
+]);
